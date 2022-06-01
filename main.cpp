a319924e9624fd97e2e33b8483d4a66fd11d993c
FuyaKamata <k021g1486@g.neec.ac.jp>
2022-06-01 12:07:31 +0900

    射影変換

--- main.cpp
+++ main.cpp
@@ -297,10 +297,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	//頂点データ
 	Vertex vertices[] = {
-		{{0.0f,100.0f,0.0f},{0.0f,1.0f}}, //左下
-		{{0.0f,0.0f,0.0f},{0.0f,0.0f}},   //左上
-		{{100.0f,100.0f,0.0f},{1.0f,1.0f}},//右下
-		{{100.0f,0.0f,0.0f},{1.0f,0.0f}},  //右上
+		{{-50.0f,-50.0f,50.0f},{0.0f,1.0f}},
+		{{-50.0f,50.0f,50.0f},{0.0f,0.0f}},
+		{{50.0f,-50.0f,50.0f},{1.0f,1.0f}},
+		{{50.0f,50.0f,50.0f},{1.0f,0.0f}},
 	};
 
 	//インデックスデータ
@@ -435,11 +435,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 	//単位行列を代入
 	constMapTransform->mat = XMMatrixIdentity();
-	//
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;//1280,720はそれぞれウィンドウの横縦幅。変数にしてマジックナンバーを避ける。
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	//行列の作成
+	//constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
+	//constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;//1280,720はそれぞれウィンドウの横縦幅。変数にしてマジックナンバーを避ける。
+	//constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
+	//constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	//平行東映行列の計算(上記の行列の作成を関数で行う)
+	constMapTransform->mat = XMMatrixOrthographicOffCenterLH(
+		0.0f, 2.0f / 1280,
+		-2.0f / 720, 0.0f,
+		0.0f, 1.0f
+	);
+	//透視投影行列の計算
+	constMapTransform->mat = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),
+		(float)1280 / 720,
+		0.1f, 1000.0f
+	);
+	//射影変換行列(透視投影)
+	XMMATRIX matProjection =
+		XMMatrixPerspectiveFovLH(
+			XMConvertToRadians(45.0f),
+			(float)1280 / 720,
+			0.1f, 1000.0f
+		);
+
+	//05_03でここでビュー変換行列(透視投影)を計算
+
+	//定数バッファに転送
+	constMapTransform->mat = matProjection;
 
 	//定数バッファのマッピング
 	ConstBufferDataMaterial* constMapMaterial = nullptr;
